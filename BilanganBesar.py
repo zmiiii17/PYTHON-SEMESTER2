09576d252a62694b6735ad27f43c0bf26d1a8a66
zmiiii17 <mhmdhazmi71@gmail.com>
2024-03-08 15:52:04 +0700

    PythonSesi4

---
+++ BilanganBesar.py
@@ -0,0 +1,9 @@
+nilai_A = int(input("Masukkan Bilangan A: "))
+nilai_B = int(input("Masukkan Bilangan B: "))
+
+if nilai_A > nilai_B:
+    hasil = "Nilai A lebih besar daripada B"
+else:
+    hasil = "Nilai B lebih besar daripada A"
+    
+print(hasil)
