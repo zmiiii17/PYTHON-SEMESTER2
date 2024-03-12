dd2af9d36740ea63846d61e1d6f43bccee6feb41
hazmiieee <153713130+zmiiii17@users.noreply.github.com>
2024-03-12 21:43:05 +0700

    PythonSesi4

---
+++ LEARNING PYTHON SESI 4/2BilanganTerbesar.py
@@ -0,0 +1,7 @@
+angka1 = int(input("Masukan Nilai satu :"))
+angka2 = int(input("Masukan nilai sua :"))
+
+if angka1 > angka2:
+    print(angka1, "Lebih besar dari pada ", angka2)
+else:
+    print(angka2, "Lebih besar dari pada ", angka1)
